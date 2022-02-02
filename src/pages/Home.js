6c430cb70d97f38bdbdb1e2849cd03821c3ad531
Joarle Mafra <joarlemafra@gmail.com>
2022-02-02 02:23:53 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { Chart } from "react-google-charts";
 import { Button, TextField } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { createUser } from '../api/apiService';
-
-
-const moment = require('moment');
+import { createUser, listBooks } from '../api/apiService';
+import moment from 'moment';
+import _ from 'lodash';
 
     function ano(){
         const startdate = moment().format('MMM-YYYY')
@@ -17,22 +16,58 @@ const moment = require('moment');
     
 function Home() {
 
+  const [options, setOptions] = useState({})
+  const [data, setData] = useState([
+    ['Livros', 'Quantidade'],
+    ['Livros Livres', 0],
+    ['Livros Alugados', 0]
+  ]);
+
+    const [totalBooks, setTotalbooks] = useState(0);
+    const [totalLoans, setTotalLoans] = useState(0);
+    const [loansGrouped, setloansGrouped] = useState({});
+    console.log(totalLoans);
+
+    async function loans(){
+      const response = await fetch('http://localhost:5000/loan');
+      const {data} = await response.json();
+      const group = data.map(loan => ({date:moment(loan.createdAt, 'YYYY-MM-DD').format('MMYY')}));
+      const loansGroupedBy = _.groupBy(group, 'date')
+      console.log('loansGroupedBy', loansGroupedBy)
+      console.log('group', group);
+      const booksHasntReturned = data.filter(({bookHasReturned})=> !bookHasReturned);
+      setTotalLoans(booksHasntReturned.length)
+  
+      console.log(data)
+    }
+
+    async function books(){
+      const {data:{count}} = await listBooks(8,0);
+      setTotalbooks(count);
+    }
+    useEffect(()=>{
+      var b = data.map(([a, b])=> {
+        if(a == 'Livros Livres') b = totalBooks-totalLoans;
+        if(a == 'Livros Alugados') b = totalLoans;
+        return [a, b]
+    })
+    setOptions({title: `Total de Livros ${totalBooks}`})
+    setData(b)
+      books()
+      loans()
+    }, [totalBooks, totalLoans]);
+
+    
+
     
     const startdate = ano();
-    const mouth1 = startdate[0].replace('-', '/');
-    const mouth2 = startdate[1].replace('-', '/');
-    const mouth3 = startdate[2].replace('-', '/');
-    const valueFake = 100
-
-    const [options, setOptions] = useState({
-      title:`Total de livros: ${valueFake}`
-    });
-    const [data, setData] = useState([
-      ['Livros', 'Quantidade'],
-      ['Livros Livres', 70],
-      ['Livros Alugados', 30]
-    ]);
+    const month1 = startdate[0].replace('-', '/');
+    const month2 = startdate[1].replace('-', '/');
+    const month3 = startdate[2].replace('-', '/');
     
+
+
+  
 
     const data2 = [
         [
@@ -46,9 +81,9 @@ function Home() {
             calc: "stringify",
           },
         ],
-        [mouth3, 8.94, "blue", null],
-        [mouth2, 10.49, "green", null],
-        [mouth1, 19.3, "gold", null],
+        [month3, 8, "blue", null],
+        [month2, 10, "green", null],
+        [month1, 19, "gold", null],
       ];
       
       const options2 = {
@@ -66,9 +101,9 @@ function Home() {
             <header className='ml-20 h-12'> 
                 <h2 class='my-4 text-purple-600 text-2xl '> Painel - AhLib </h2>
             </header>
-          <div className=' flex justify-end' style={{display: "flex", position: 'absolute'}}>
-             
+          <div className='flex justify-end' style={{display: "flex", position: 'absolute'}}>
             <Chart
+            bg-current 
                 fontSize={'100'}
                 width={'500px'}
                 height={'300px'}
@@ -78,6 +113,8 @@ function Home() {
                 data={data}
                 options={options}
             />
+
+     
             <Chart
                 chartType="BarChart"
                 width="100%"
@@ -85,10 +122,6 @@ function Home() {
                 data={data2}
                 options={options2}
           />
-            <div className=''>
-                <Button variant='contained' type='submit'>Livros</Button>
-
-    </div>
           </div>
 
           
