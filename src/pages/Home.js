116d729130f8d73f07bf2438b2602dc2a30d0e91
Joarle Mafra <joarlemafra@gmail.com>
2022-02-03 08:56:00 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,21 +7,16 @@ import { createUser, listBooks } from '../api/apiService';
 import moment from 'moment';
 import _ from 'lodash';
 
-function ano() {
-  const startdate = moment().format('MMM-YYYY')
-  const startdate1 = moment().subtract(1, 'months').format('MMM-YYYY');
-  const startdate2 = moment().subtract(2, 'months').format('MMM-YYYY');
-  return [startdate, startdate1, startdate2]
-}
-
 function Home() {
+  const month1 = moment().format('MMM/YYYY')
+  const month2 = moment().subtract(1, 'months').format('MMM/YYYY');
+  const month3 = moment().subtract(2, 'months').format('MMM/YYYY');
 
 
-  const startdate = ano();
-  const month1 = startdate[0].replace('-', '/');
-  const month2 = startdate[1].replace('-', '/');
-  const month3 = startdate[2].replace('-', '/');
-  const valueFake = 100
+  // const month1 = startdate[0].replace('-', '/');
+  // const month2 = startdate[1].replace('-', '/');
+  // const month3 = startdate[2].replace('-', '/');
+  // const valueFake = 100
 
   const [options, setOptions] = useState({})
   const [data, setData] = useState([
@@ -33,19 +28,51 @@ function Home() {
   const [totalBooks, setTotalbooks] = useState(0);
   const [totalLoans, setTotalLoans] = useState(0);
   const [loansGrouped, setloansGrouped] = useState({});
-  console.log(totalLoans);
+
+  const [option2, setOption2] = useState(
+    [
+      [
+        "Elemento",
+        "Quantidade",
+        { role: "style" },
+        {
+          sourceColumn: 0,
+          role: "annotation",
+          type: "number",
+          calc: 'stringfy',
+          bars: 'horizontal',
+          backgroundColor: '#E4E4E4'
+
+        },
+      ],
+      [month3, 0, "blue", null],
+      [month2, 0, "green", null],
+      [month1, 0, "gold", null],
+    ]
+  )
+
 
   async function loans() {
     const response = await fetch('http://localhost:5000/loan');
     const { data } = await response.json();
-    const group = data.map(loan => ({ date: moment(loan.createdAt, 'YYYY-MM-DD').format('MMYY') }));
-    const loansGroupedBy = _.groupBy(group, 'date')
-    console.log('loansGroupedBy', loansGroupedBy)
-    console.log('group', group);
+    const group = data.map(loan => ({ date: moment(loan.createdAt, 'YYYY-MM-DD').format('MMM/YYYY') }));
+    // const loansGroupedBy = _.groupBy(group, 'date')
+    const teste = Object.entries(_.groupBy(group, 'date')).reduce((acc, [key, value]) => {
+      acc[key] = value.length;
+      return acc;
+      
+    }, {})
+    console.log('teste', teste)
+    
+    var b = option2.map(([a,b,c,d]) => {
+      
+      if(teste[a]) b = teste[a];
+      return [a, b, c,  d]
+})
+  setOption2(b)
     const booksHasntReturned = data.filter(({ bookHasReturned }) => !bookHasReturned);
     setTotalLoans(booksHasntReturned.length)
 
-    console.log(data)
   }
 
   async function books() {
@@ -58,28 +85,15 @@ function Home() {
       if (a == 'Livros Alugados') b = totalLoans;
       return [a, b]
     })
-    setOptions({ title: `Total de Livros ${totalBooks}` })
+    setOptions({ title: `Total de Livros ${totalBooks}`,
+    backgroundColor: '#E4E4E4',
+  })
     setData(b)
     books()
     loans()
   }, [totalBooks, totalLoans]);
 
-  const data2 = [
-    [
-      "Element",
-      "Density",
-      { role: "style" },
-      {
-        sourceColumn: 0,
-        role: "annotation",
-        type: "string",
-        calc: "stringify",
-      },
-    ],
-    [month3, 8, "blue", null],
-    [month2, 10, "green", null],
-    [month1, 19, "gold", null],
-  ];
+
 
   const options2 = {
     title: "Emprestimos por Periodo",
@@ -87,18 +101,23 @@ function Home() {
     height: 250,
     bar: { groupWidth: "95%" },
     legend: { position: "none" },
+    backgroundColor: '#E4E4E4',
   };
 
 
 
   return (
-    <div className='container mx-auto'>
-      <header className='ml-20 h-12'>
+
+      <div className='h-12'>
+    <div>
+      <header className='ml-20'>
         <h2 class='my-4 text-purple-600 text-2xl '> Painel - AhLib </h2>
       </header>
+  </div>
 
-
-      <div className='flex justify-end' style={{ display: "flex", position: 'absolute' }}>
+      <div className='justify-center' style={{ display: "flex", position: '' }}>
+        <div className='justify-center' style={{ display: "flex", position: '' }}>
+      <div className='mr-10'>
         <Chart
           bg-current
           fontSize={'100'}
@@ -109,20 +128,23 @@ function Home() {
           loader={<div>Carregando Painel</div>}
           data={data}
           options={options}
-        />
-
-
+          />
+          </div>
+          <div>
         <Chart
           chartType="BarChart"
           width="100%"
-          height="400px"
-          data={data2}
+          height="300px"
+          data={option2}
           options={options2}
-        />
+          />
+          </div>
+          
+          </div>
       </div>
 
-
-    </div>
+</div>
+   
   );
 }
 
