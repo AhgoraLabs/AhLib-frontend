641e2ec0702ce3cb4b5d1c6b4d4dec16cad30a66
Joarle Mafra <joarlemafra@gmail.com>
2022-02-04 00:59:54 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,13 @@ import { useForm } from 'react-hook-form';
 import { createUser, listBooks } from '../api/apiService';
 import moment from 'moment';
 import _ from 'lodash';
+import qrCode from '../images/qrCodeApp.png';
+import { height } from '@mui/system';
+
+const getChartSize = (windowSize) => ({
+   [windowSize < 1000]: [10, 5], 
+   [windowSize > 1000 && windowSize < 1700]: [800, 500] 
+}).true || [1500, 1000]
 
 function Home() {
   const month1 = moment().format('MMM/YYYY')
@@ -28,6 +35,12 @@ function Home() {
   const [totalBooks, setTotalbooks] = useState(0);
   const [totalLoans, setTotalLoans] = useState(0);
   const [loansGrouped, setloansGrouped] = useState({});
+  const [chartHeight, setChartHeight] = useState(0);
+  const [chartWidth, setChartWidth] = useState(0);
+  const [windowSize, setwindowSize] = useState(0);
+
+
+
 
   const [option2, setOption2] = useState(
     [
@@ -41,7 +54,6 @@ function Home() {
           type: "number",
           calc: 'stringfy',
           bars: 'horizontal',
-          backgroundColor: '#E4E4E4'
 
         },
       ],
@@ -80,20 +92,32 @@ function Home() {
     setTotalbooks(count);
   }
   useEffect(() => {
+    window.addEventListener('resize', ()=>{
+        const {innerWidth} = window;
+        setwindowSize(innerWidth)
+        const [width, height] = getChartSize(innerWidth)
+        setChartWidth(width)
+        setChartHeight(height)
+        console.log('teste')
+    }) 
+     
     var b = data.map(([a, b]) => {
       if (a == 'Livros Livres') b = totalBooks - totalLoans;
       if (a == 'Livros Alugados') b = totalLoans;
       return [a, b]
     })
-    setOptions({ title: `Total de Livros ${totalBooks}`,
-    backgroundColor: '#E4E4E4',
-  })
+    setOptions({ title: `Total de Livros ${totalBooks}`})
+    const {innerWidth} = window;
+    setwindowSize(innerWidth)
+    const [width, height] = getChartSize(innerWidth)
+    setChartWidth(width)
+    setChartHeight(height)
     setData(b)
     books()
     loans()
   }, [totalBooks, totalLoans]);
 
-
+  
 
   const options2 = {
     title: "Emprestimos por Periodo",
@@ -101,7 +125,6 @@ function Home() {
     height: 250,
     bar: { groupWidth: "95%" },
     legend: { position: "none" },
-    backgroundColor: '#E4E4E4',
   };
 
 
@@ -109,20 +132,20 @@ function Home() {
   return (
 
       <div className='h-12'>
-    <div>
+        <div>
       <header className='ml-20'>
         <h2 class='my-4 text-purple-600 text-2xl '> Painel - AhLib </h2>
       </header>
   </div>
 
-      <div className='justify-center' style={{ display: "flex", position: '' }}>
-        <div className='justify-center' style={{ display: "flex", position: '' }}>
+      <div className='justify-center flex'>
+        <div className='justify-center flex'>
       <div className='mr-10'>
         <Chart
           bg-current
           fontSize={'100'}
-          width={'500px'}
-          height={'300px'}
+          width={chartWidth}
+          height={chartHeight}
           chartType="PieChart"
           graphID="ScatterChart"
           loader={<div>Carregando Painel</div>}
@@ -134,15 +157,20 @@ function Home() {
         <Chart
           chartType="BarChart"
           width="100%"
-          height="300px"
+          height="100%"
           data={option2}
           options={options2}
-          />
+        />
           </div>
           
           </div>
       </div>
-
+    <div className='justify-center flex flex-nowrap' >
+      <div className='justify-center text-center text-lg'>
+      <h1 className='h-10'>Acesse pelo App</h1> 
+      <img alt='qrCode' src={qrCode} width={'250px'}/>
+      </div>
+    </div>
 </div>
    
   );
