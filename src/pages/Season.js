066d342fe4b3f6fc05a0fdae821a91ccb12b8342
Pedro <pedropverani@hotmail.com>
2022-02-10 00:45:19 -0300

    fixing season

--- src/pages/Season.js
+++ src/pages/Season.js
@@ -6,14 +6,17 @@ import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/auth/AuthContext';
 import Button from '../components/Button';
+import moment from  'moment';
 import { getRecommendation, getUser } from '../api/apiService';
 
 const Season = () => {
 
     const [recommendations, setRecommendation] = useState([]);
+
     const [user, setUser] = useState();
 
     const { user: { email } } = useContext(AuthContext);
+
 
     const fetchUser = async () => await getUser(email);
     const fetchRecommendation = async () => await getRecommendation();
@@ -23,12 +26,13 @@ const Season = () => {
         const  user =  email ?  await fetchUser() : '';
         setUser(user.data);
         setRecommendation(data);
-    }, [])
+    }, [email])
 
     return (
         <div className="flex justify-center flex-col items-center h-full w-screen font-poppins">
             <div className="w-4/5 h-full">
                 {recommendations?.map(({ bookRecommended, description, user, seasonEndDate }) => {
+                    console.log(seasonEndDate, 'seasonEnd')
                     return (
                         <div className='flex m-4 sm:h-card h-96 justify-between shadow-lg rounded-xl border-gray-100'>
                             <div className='w-1/5 flex h-full justify-center items-center lg:flex hidden'>
@@ -46,11 +50,11 @@ const Season = () => {
                                 <div className='w-full sm:h-1/4 sm:mt-4 mt-2 flex sm:justify-end justify-center items-end '>
                                     <div className='lg:h-full flex justify-between w-full items-center md:p-0 p-4 mb-2 md:text-base text-sm sm:flex-row flex-col translate-x-2.5 translate-y-7'>
                                         <div>
-                                            <span>Recomendação até  {new Date(seasonEndDate).toLocaleDateString()}</span>
+                                            <span>Recomendação até  {moment(seasonEndDate).format('DD-MM-YYYY')}</span>
                                         </div>
                                         <div className='lg:flex-col flex flex-row'>
                                             <span>{user.name}</span>
-                                            <span className='text-right ml-4'>{'DSW'}</span>
+                                            <span className='text-right ml-4'>{user.department}</span>
                                         </div>
 
                                     </div>
@@ -58,7 +62,7 @@ const Season = () => {
                                 </div>
                             </div>
                             <div className='w-18 flex justify-end items-end lg:flex hidden'>
-                                <img className='m-4 w-3/4 rounded-full h-1/4' src={'https://conteudo.imguol.com.br/blogs/174/files/2018/05/iStock-648229868-1024x909.jpg'} />
+                                <img className='m-4 w-3/4 rounded-full h-1/4' src={user.pictureUrl || 'https://conteudo.imguol.com.br/blogs/174/files/2018/05/iStock-648229868-1024x909.jpg'} />
                             </div>
                         </div>
                     );
