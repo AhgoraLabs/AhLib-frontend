bb963bfba1e6ec776ec2654b9e369695725ad41c
Pedro <pedropverani@hotmail.com>
2022-02-10 01:55:41 -0300

    fixing login

--- src/routes/NavRoute.js
+++ src/routes/NavRoute.js
@@ -5,6 +5,7 @@ import { Route, Redirect } from "react-router-dom";
 export const NavRoute = ({ exact, path, component: Component, header: Header }) => {
 
     const isLogged = !!localStorage.getItem('@App:token');
+    console.log(isLogged, window.location.path)
 
     const render = isLogged ?
         <Route exact={exact} path={path} render={(props) => (
