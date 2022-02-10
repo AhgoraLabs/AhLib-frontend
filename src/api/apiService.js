82badaae2f4eb256521c0c56d3b673822328952c
Pedro <pedropverani@hotmail.com>
2022-02-10 01:47:06 -0300

    fixing url

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 
+const urlBase = !window.location.host.includes('netlify') ? 'http://localhost:5000' : 'http://sound-aileron-337523.rj.r.appspot.com/'
+
+
 axios.defaults.headers.post['auth'] = window.localStorage.getItem('@App:token');
 axios.defaults.headers.get['auth'] = window.localStorage.getItem('@App:token');
 axios.defaults.headers.patch['auth'] = window.localStorage.getItem('@App:token');
@@ -8,32 +11,32 @@ axios.defaults.headers.delete['auth'] = window.localStorage.getItem('@App:token'
 axios.defaults.headers.common['Authorization'] = window.localStorage.getItem('@App:token');
 
 export const createBook = async (book) => {
-    const data = await axios.post('http://localhost:5000/books/', book);
+    const data = await axios.post(`${urlBase}/books/`, book);
     return data;
 }
 
 export const editBook = async (book) => {
-    const data = await axios.patch('http://localhost:5000/books/', book);
+    const data = await axios.patch(`${urlBase}/books/`, book);
     return data;
 }
 export const removeBook = async (id) => {
-    const data = await axios.delete(`http://localhost:5000/books/${id}`);
+    const data = await axios.delete(`${urlBase}/books/${id}`);
     return data;
 }
 
 
 export const listBooks = async (limit, offset) => {
-    const data = await axios.get(`http://localhost:5000/books?limit=${limit}&offset=${offset}`);
+    const data = await axios.get(`${urlBase}/books?limit=${limit}&offset=${offset}`);
     return data;
 }
 
 export const getBookByTitle = async (title) => {
-    const { data: { data } } = await axios.get(`http://localhost:5000/books/book/${title}`)
+    const { data: { data } } = await axios.get(`${urlBase}/books/book/${title}`)
     return data;
 }
 
 export const getBookById = async (id) => {
-    const { data: { data } } = await axios.get(`http://localhost:5000/books/${id}`)
+    const { data: { data } } = await axios.get(`${urlBase}/books/${id}`)
     return data;
 }
 
@@ -48,7 +51,7 @@ export const authUser = async (email, password) => {
             }
         }
 
-        const response = await fetch('http://localhost:5000/users/auth', settings);
+        const response = await fetch(`${urlBase}/users/auth`, settings);
         const data = await response.json();
         console.log(data, 'dataapi')
 
@@ -68,7 +71,7 @@ export const createUser = async (email, name) => {
         }
     }
     try {
-        const response = await fetch('http://localhost:5000/users/create', settings);
+        const response = await fetch(`${urlBase}/users/create`, settings);
         const data = await response.json();
         return data;
     } catch (error) {
@@ -77,67 +80,67 @@ export const createUser = async (email, name) => {
 }
 
 export const getUser = async (email) => {
-    const { data } = await axios.get(`http://localhost:5000/users/user?email=${email}`);
+    const { data } = await axios.get(`${urlBase}/users/user?email=${email}`);
     return data;
 }
 
 export const getUsers = async () => {
-    const { data: { data } } = await axios.get('http://localhost:5000/users');
+    const { data: { data } } = await axios.get(`${urlBase}/users`);
     return data;
 };
 
 export const createSuggestion = async (suggestionData) => {
-    const data = await axios.post('http://localhost:5000/', suggestionData);
+    const data = await axios.post(`${urlBase}/suggestion`, suggestionData);
     return data;
 }
 
 export const createLoan = async ({ bookId, person, loanEnd }) => {
-    const data = await axios.post('http://localhost:5000/loan', { bookId, person, loanEnd });
+    const data = await axios.post(`${urlBase}/loan`, { bookId, person, loanEnd });
     return data;
 };
 
 export const getLoanByBookId = async (id) => {
-    const { data: { data } } = await axios.get(`http://localhost:5000/loan/book/${id}`)
+    const { data: { data } } = await axios.get(`${urlBase}/loan/book/${id}`)
     return data;
 }
 
 export const getAllLoansByBookId = async (id) => {
-    const { data: { data } } = await axios.get(`http://localhost:5000/loan/count/${id}`)
+    const { data: { data } } = await axios.get(`${urlBase}/loan/count/${id}`)
     return data
 }
 
 export const getAllLoan = async (id) => {
-    const { data: { data } } = await axios.get(`http://localhost:5000/loan`)
+    const { data: { data } } = await axios.get(`${urlBase}/loan`)
     return data;
 }
 
 export const endLoan = async (id) => {
-    const data = await axios.patch(`http://localhost:5000/loan/${id}`, { bookHasReturned: true })
+    const data = await axios.patch(`${urlBase}/loan/${id}`, { bookHasReturned: true })
     return data;
 }
 
 export const extendLoan = async (id, newDate) => {
 
-    const data = await axios.patch(`http://localhost:5000/loan/${id}`, { newLoanEnd: newDate, loanEndHasBeenExtended: true })
+    const data = await axios.patch(`${urlBase}/loan/${id}`, { newLoanEnd: newDate, loanEndHasBeenExtended: true })
     return data;
 }
 
 export const getCommentsById = async (bookId) => {
-    const { data: { data } } = await axios.get(`http://localhost:5000/comments/?idBook=${bookId}`)
+    const { data: { data } } = await axios.get(`${urlBase}/comments/?idBook=${bookId}`)
     return data;
 };
 
 export const createComment = async (comment) => {
-    const { data: { data } } = await axios.post(`http://localhost:5000/comments`, comment)
+    const { data: { data } } = await axios.post(`${urlBase}/comments`, comment)
     return data;
 };
 
 export const getRecommendation = async () => {
-    const { data } = await axios.get(`http://localhost:5000/season`);
+    const { data } = await axios.get(`${urlBase}/season`);
     return data;
-} 
+}
 
 export const createRecommendation = async (recommendation) => {
-    const data = await axios.post(`http://localhost:5000/season` , recommendation)
+    const data = await axios.post(`${urlBase}/season`, recommendation)
     return data;
 }
