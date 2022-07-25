f525c8a6f40fde645abdc02d623f4a9c1b8b8bce
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2022-07-25 20:42:53 -0300

    ♻️ Refatorando e remanejando componente Box da listagem de livro

--- src/pages/ListBooks/components/Box.js
+++ src/pages/ListBooks/components/Box.js
@@ -1,29 +1,61 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import Rating from "@mui/material/Rating";
-import { getCommentsById, getLoanByBookId } from "../api/apiService";
-import { totalStars } from "../utils/totalStar";
 import CircularProgress from "@mui/material/CircularProgress";
 
-import NoImage from "../components/NoImageBook";
+import NoImage from "../../../components/NoImageBook";
+
+import styled from "styled-components";
+
+const BoxContainer = styled.div`
+    border: 1px solid lightgray;
+    width: 13rem;
+    height: 20rem;
+    border-radius: 10px;
+    margin: 10px;
+    box-shadow: 0px 10px 5px 2px lightgray;
+    margin: 35px 25px;
+    cursor: pointer;
+    transition: all 0.5s;
+    background-color: ghostwhite;
+
+    &:hover {
+        border: 2px solid lightgray;
+        transform: translateY(-8px);
+    }
+
+    #imageBook {
+        width: 140px;
+        height: 220px;
+        border-radius: 10px;
+        position: relative;
+        top: 0;
+        margin-top: -40px;
+        margin-bottom: 10px;
+    }
+
+    #titleBook {
+        color: black;
+        text-transform: capitalize;
+        cursor: pointer;
+    }
+`;
+
 
 const Box = ({ title, image, bookId, averageStars, isBookLoaned }) => {
-    const [loan, setLoan] = useState({});
-    const [votes, setVotes] = useState([]);
     const [isLoading, setLoading] = useState(true);
-
 
     setTimeout(() => {
         setLoading(false);
     }, 600);
 
     return (
-        <div onClick={() => (window.location = `/livros/info/${bookId}`)} className="h-80 w-56 m-4 mx-10 cursor-pointer bg-white border border-gray-100 rounded-3xl shadow-input-box-shadow">
+        <BoxContainer onClick={() => (window.location = `/livros/info/${bookId}`)} >
             <div className="flex justify-center">
-                {image ? <div style={{ backgroundImage: `url(${image})` }} className="flex justify-center h-48 w-44 mt-4 bg-no-repeat bg-contain bg-center "></div> : <NoImage title={title} />}
+                {image ?  <img id="imageBook" src={image} /> : <NoImage title={title} />}
             </div>
-            <div className={`truncate text-center m-1 flex  flex-col items-center ${title.length > 28 ? "text-xs capitalize break-normal" : " capitalize"}`}>
-                <label>{title.split(":")[0]}</label>
+
+            <div className={` text-center m-1`}> 
                 <div>
                     {isLoading ? (
                         <div className="font-thin flex items-center justify-center">
@@ -33,9 +65,10 @@ const Box = ({ title, image, bookId, averageStars, isBookLoaned }) => {
                         <div className={`${isBookLoaned ? "text-base font-thin text-danger font-medium" : " text-base font-thin text-textbook font-medium"} text-center `}>{isBookLoaned ? "alugado" : "Livre"}</div>
                     )}
                 </div>
+                <label id="titleBook"> {title.split(":")[0]}</label>
                 {averageStars > 0 && <Rating name="half-rating-read" defaultValue={averageStars} precision={0.5} readOnly />}
             </div>
-        </div>
+        </BoxContainer>
     );
 };
 
