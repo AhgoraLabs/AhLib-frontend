a0875f5ce7fd498c97b5858523daa0981e7d42c1
Pedro Verani <pedropverani@hotmail.com>
2022-07-29 13:36:11 -0300

    adding feedback

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,8 @@ import { Button } from '@mui/material';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 import AuthContext from '../context/auth/AuthContext';
+import { redirectWithMsg } from "../utils/helpers";
+
 
 const urlBase = !window.location.host.includes('netlify') ? 'http://localhost:5000' : 'https://ahlib.herokuapp.com'
 const styleInput = `h-16 px-4 mt-5 outline-none border-2 border-gray-400 rounded-xl focus:border-blue-700 font-sans`;
@@ -21,6 +23,9 @@ const ProfilePage = () => {
 
     const onSubmit = async (dataForm) => {
         const response = await axios.patch(`${urlBase}/users`, dataForm);
+        if(response.status === 200) {
+            redirectWithMsg('/livros', 'success', 'Senha atualizada com sucesso!');
+        }
     };
     const nomeUsuario = user.name;
 
