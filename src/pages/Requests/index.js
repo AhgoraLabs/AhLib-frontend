061e28c0efa80a3e92f29e505f6adb5763fc2fc3
Pedro Verani <pedropverani@hotmail.com>
2022-08-04 10:36:42 -0300

    fixing request

--- src/pages/Requests/index.js
+++ src/pages/Requests/index.js
@@ -1,33 +1,39 @@
 import React, { useEffect, useState, useContext } from "react";
-import {Container} from './style'
+import { Container } from './style'
 import Button from '../../components/Button'
-import {fetchRequest} from '../../api/apiService' 
+import { fetchRequest } from '../../api/apiService'
 import AuthContext from "../../context/auth/AuthContext";
 import moment from 'moment'
 
 import { SnackbarProvider, useSnackbar } from 'notistack'
 
-import {AiOutlineBook} from "react-icons/ai";
-import {BsPerson, BsCalendarDate} from "react-icons/bs";
+import { AiOutlineBook } from "react-icons/ai";
+import { BsPerson, BsCalendarDate } from "react-icons/bs";
 
 const Requests = ({ }) => {
-   
+
     const [dataRequests, setDataRequests] = useState([])
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
     const { user: { id: userId } } = useContext(AuthContext);
 
-    const approveRequests = async ({id, type = 'approve'}) => {
-        const response = await fetchRequest({url: `/request/${type}`, method: 'PUT', data: {
-            id, userId
-        }})
+    const approveRequests = async ({ id, type = 'approve' }) => {
+        const response = await fetchRequest({
+            url: `/request/${type}`, method: 'PUT', data: {
+                id, userId
+            }
+        })
+        mountPendingList(   );
 
-        enqueueSnackbar(`Solicitação ${type === 'approve' ? ' aprovada ': ' recusada' } com sucesso.`, { variant: `success` })
+        enqueueSnackbar(`Solicitação ${type === 'approve' ? ' aprovada ' : ' recusada'} com sucesso.`, { variant: `success` })
     }
 
-    useEffect(async () => {
-        const data = await fetchRequest({url: '/request', method: 'GET'})
+    const mountPendingList = async () => {
+        const data = await fetchRequest({ url: '/request/pending', method: 'GET' })
         setDataRequests(data)
+    }
+    useEffect(async () => {
+        mountPendingList();
     }, []);
 
     return (
@@ -37,33 +43,33 @@ const Requests = ({ }) => {
                 <Button height='h-8' fontSize='text-l' styleCustom='m-4'><h1>Recusadas</h1></Button>
             </div>
             <div id='table'>
-                {  
-                    dataRequests && 
-                    dataRequests.map((elem, index) => 
-                        (
-                            <div className='card' key={index}>
-                                <div className='card-left'>
-                                    <span className='person-name'>
-                                        <BsPerson size={18}/>
-                                        Nome: {elem.userName || elem.userId}
-                                        </span>
-                                    <span className='book-name'>
-                                        <AiOutlineBook size={18}/>
-                                        Livro: {elem.bookName || elem.bookId}
-                                    </span>
-                                    <span className='date'>
-                                        <BsCalendarDate size={18} style={{marginRight: 3}}/>
-                                        Solicitado Dia: {moment(elem.createdAt).format("DD/MM/YYYY")}
-                                    </span>
-                                </div>
-                                <div className='card-right'>
-                                    <div className='div-buttons'>
-                                        <Button onClick={() => approveRequests({id: elem._id, type: 'approve'})} background='bg-green-700' height='h-6' width='w-18' fontSize='text-sm' styleCustom='m-4'><h1>Aprovar</h1></Button>
-                                        <Button onClick={() => approveRequests({id: elem._id, type: 'decline'})} background='bg-red-700' height='h-6' width='w-18' fontSize='text-sm' styleCustom='m-4'><h1>Recusar</h1></Button>
-                                    </div>
+                {
+                    dataRequests &&
+                    dataRequests.map((elem, index) =>
+                    (
+                        <div className='card' key={index}>
+                            <div className='card-left'>
+                                <span className='person-name'>
+                                    <BsPerson size={18} />
+                                    Nome: {elem.userName || elem.userId}
+                                </span>
+                                <span className='book-name'>
+                                    <AiOutlineBook size={18} />
+                                    Livro: {elem.bookName || elem.bookId}
+                                </span>
+                                <span className='date'>
+                                    <BsCalendarDate size={18} style={{ marginRight: 3 }} />
+                                    Solicitado Dia: {moment(elem.createdAt).format("DD/MM/YYYY")}
+                                </span>
+                            </div>
+                            <div className='card-right'>
+                                <div className='div-buttons'>
+                                    <Button onClick={() => approveRequests({ id: elem._id, type: 'approve' })} background='bg-green-700' height='h-6' width='w-18' fontSize='text-sm' styleCustom='m-4'><h1>Aprovar</h1></Button>
+                                    <Button onClick={() => approveRequests({ id: elem._id, type: 'decline' })} background='bg-red-700' height='h-6' width='w-18' fontSize='text-sm' styleCustom='m-4'><h1>Recusar</h1></Button>
                                 </div>
                             </div>
-                        ))
+                        </div>
+                    ))
                 }
 
             </div>
